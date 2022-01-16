3142103766af810198b0aecb42e3b4dd6025b230
PASUPULETI <raviteja.pasupuleti@dxc.com>
2022-01-16 17:48:06 +0530

    This is a commit

---
+++ Ternary Operator/src/Ternary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ternary {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        Ternary_Operator(n);
+        int length = scan.nextInt();
+        int breadth = scan.nextInt();
+        Area_of_rectangle(length,breadth);
+    }
+    public static void Ternary_Operator(int n){
+        String x = (n>=5)?(n>5?"Correct":"Not ok"):(n>3?"Ok":"Invalid");
+        System.out.println(x);
+    }
+    public static void Area_of_rectangle(int length, int breadth){
+        int Area = length * breadth;
+        System.out.println("Area of a rectangle is: "+Area);
+    }
+}

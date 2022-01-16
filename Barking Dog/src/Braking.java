1dc60d012578d8f377e604b0cec63418b646b5c0
PASUPULETI <raviteja.pasupuleti@dxc.com>
2022-01-16 21:03:19 +0530

    This is a commit

--- Barking Dog/src/Braking.java
+++ Barking Dog/src/Braking.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Braking {
-    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        boolean barking = scan.nextBoolean();
+        int HourOfDay = scan.nextInt();
+        boolean result = ShouldWakeUp(barking,HourOfDay);
+        System.out.println(result);
+    }
+    public static boolean ShouldWakeUp(boolean barking, int HourOfDay){
+        if(HourOfDay>0 && HourOfDay<=23){
+            if(barking && (HourOfDay<8 || HourOfDay>22)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
 }
